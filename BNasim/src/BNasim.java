5321cb966df12f38af8ea6c24315a802b5277084
nasim ahadi <nasim2ahadi@gmail.com>
2015-05-31 08:05:36 -0600

    I am not sure how to revise the same file. a little confusing

---
+++ BNasim/src/BNasim.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+public class BNasim {
+	
+
+
+		public static void main (String [] args)
+				throws java.io.IOException
+		{
+			int number, result;
+			int remain;			
+			System.out.print("Decimal: ");
+			Scanner s=new Scanner(System.in);
+			number = s.nextInt();
+			if(number>0){
+			number = number/2;
+			remain = number%2;
+			
+			System.out.println(remain);}
+			if(number>0){
+				number = number/2;
+				remain = number%2;
+				
+				System.out.println(remain);}
+			if(number>0){
+				number = number/2;
+				remain = number%2;
+				
+				System.out.println(remain);}
+			if(number>0){
+				number = number/2;
+				remain = number%2;
+				
+				System.out.println(remain);}
+			if(number>0){
+				number = number/2;
+				remain = number%2;
+				System.out.println(number);
+				System.out.println(remain);}
+			if(number>0){
+				number = number/2;
+				remain = number%2;
+				System.out.println(number);
+				System.out.println(remain);}
+			if(number>0){
+				number = number/2;
+				remain = number%2;
+				System.out.println(number);
+				System.out.println(remain);}
+			if(number>0){
+				number = number/2;
+				remain = number%2;
+				System.out.println(remain);}
+			if(number>0){
+				number = number/2;
+				remain = number%2;
+				System.out.println(remain);}
+			if(number>0){
+				number = number/2;
+				remain = number%2;
+				
+				System.out.println(remain);}
+			if(number>0){
+				number = number/2;
+				remain = number%2;
+				
+				System.out.println(remain);}
+		
+						
+			
+			}
+			}
+	
+			
+	
+			
+		
+	
+
+	
+
